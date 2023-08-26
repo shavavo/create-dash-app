1e348025b56aec673959c1b65230fd8d75045dc9
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-08-26 07:35:21 -0700

    Update create-react-app.py

--- create-react-app.py
+++ create-react-app.py
@@ -1,23 +1,14 @@
-import os
-import shutil
-import glob
-import subprocess
+import os, shutil, glob, subprocess
 
-name = input("Application name (subdomain): ")
-ssh = input("SSH Endpoint (ie. [NET_ID]@netvisdev.fuqua.duke.edu): ")
-port = input("Port: ")
-
-project_dir = '../{}-DEV/'.format(name)
-deploy_dir = '../{}-DEPLOY/'.format(name)
+name, ssh, port = input("Application name (subdomain): "), input("SSH Endpoint (ie. [NET_ID]@netvisdev.fuqua.duke.edu): "), input("Port: ")
+project_dir, deploy_dir = '../{}-DEV/'.format(name), '../{}-DEPLOY/'.format(name)
 
 shutil.copytree('package/DEV/', project_dir, symlinks=False, ignore=None)
 shutil.copytree('package/DEPLOY/', deploy_dir, symlinks=False, ignore=None)
 
 for directory in [project_dir, deploy_dir]:
     for filename in glob.iglob(directory + '**/*.sh', recursive=True):
-        generator_dir = os.path.dirname(filename)
-        generated_name = filename.split('_')[-1][0:-3]
-
+        generator_dir, generated_name = os.path.dirname(filename), filename.split('_')[-1][0:-3]
         subprocess.call(['bash {} > {}/{} {} {} {}'.format(filename, generator_dir, generated_name, name, port, ssh)], shell=True)
         os.remove(filename)
 
